9ca6179dccd863dc087f0f706c3167f7826dfd8a
Jack ben <335864012@qq.com>
2019-05-21 22:59:48 +0800

    加入对象基类

---
+++ WTL1/drawBase.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "drawBase.h"
+
+
+drawBase::drawBase()
+{
+}
+
+
+drawBase::~drawBase()
+{
+}
--- WTL1/resource.h
+++ WTL1/resource.h
@@ -1,20 +1,19 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by WTL1.RC
+// Microsoft Visual C++ 生成的包含文件。
+// 供 WTL1.rc 使用
 //
-
-
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-//#define IDR_WTL1TYPE	129
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define ID_DRAW_LINE                    32775
+#define ID_BUTTON32776                  32776
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	201
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32775
+#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
